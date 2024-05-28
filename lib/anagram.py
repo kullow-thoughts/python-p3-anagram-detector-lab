2c19ce974fc80413b1a6fb538c18c9878496ad4a
Abdulhakimkullow <abdulhakim.kullow@student.moringaschool.com>
2024-05-28 19:56:01 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self,word_listing):
+        matched_words=[]
+        for word in word_listing:
+            if sorted(word)== sorted(self.word):
+                matched_words.append(word)
+        return matched_words
+        
